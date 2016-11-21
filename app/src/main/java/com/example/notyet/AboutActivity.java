b819666ec5b26c2ea97a550032bb9908a651bcfa
Neil Joyer <Neil Joyer>
2016-11-21 16:41:04 -0600

    Add email link to About page.

--- app/src/main/java/com/example/notyet/AboutActivity.java
+++ app/src/main/java/com/example/notyet/AboutActivity.java
@@ -3,7 +3,6 @@ package com.example.notyet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
-import android.text.util.Linkify;
 import android.widget.TextView;
 
 // Information to the user about this application and the application's attributions.
@@ -14,8 +13,11 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        TextView aboutAttributionGraph = (TextView)findViewById(R.id.about_attribution_graph);
+        TextView aboutEmail = (TextView)findViewById(R.id.about_email);
+        aboutEmail.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView aboutAttribution = (TextView)findViewById(R.id.about_attribution);
         //To make the links clickable
-        aboutAttributionGraph.setMovementMethod(LinkMovementMethod.getInstance());
+        aboutAttribution.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
